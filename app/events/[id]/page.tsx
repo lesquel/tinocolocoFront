8fadd808d5ade67fb94da83980590487fbc65349
lesquel <holahi23holahi23@gmail.com>
2024-12-30 06:36:58 -0500

    aa

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -3,15 +3,14 @@ import { Section } from '@/components/sections/layout/section';
 import { SectionReview } from '@/features/events/section/events/review/secitonReview';
 import EventCard from '@/features/events/components/EventCard';
 
-// Use the correct type from Next.js or ensure compatibility with dynamic routing
 interface EventPageProps {
   params: {
     id: string;
   };
 }
 
-export default function Event({ params }: { params: { id: string } }) {
-  const eventId = parseInt(params.id, 10); // Convert string ID to a number
+export default function Event({ params }: EventPageProps) {
+  const eventId = parseInt(params.id, 10);
 
   return (
     <Container>
