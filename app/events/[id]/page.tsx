eecc43aa544d2f861b929a8c089f3540b9efd2bd
lesquel <holahi23holahi23@gmail.com>
2024-12-30 06:25:55 -0500

    first commit

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,29 +1,23 @@
-// app/events/[id]/page.tsx
-
 import { Container } from '@/components/sections/layout/container';
 import { Section } from '@/components/sections/layout/section';
 import { SectionReview } from '@/features/events/section/events/review/secitonReview';
 import EventCard from '@/features/events/components/EventCard';
 
-// Define the type for the page props
 interface EventPageProps {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }
 
-const Event = ({ params }: EventPageProps) => {
+export default function Event({ params }: EventPageProps) {
   const eventId = parseInt(params.id, 10);
-
-  console.log('eventId', eventId);
 
   return (
     <Container>
       <Section>
         <EventCard id={eventId} />
       </Section>
-
       <SectionReview eventId={eventId} />
     </Container>
   );
-};
-
-export default Event;
+}
